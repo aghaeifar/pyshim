8ba4d2d528a1c88fee2d56d09c4e7c3a00aae609
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2024-01-16 10:58:46 +0100

    minor

--- pyshim/b0.py
+++ pyshim/b0.py
@@ -2,23 +2,23 @@
 import torch
 import numpy as np
 from recotwix.sequences import recoB0
-from shimming import shimming_base
+from . import shimming_base
 
 class shimming_b0(shimming_base):
     _b0_obj = None
 
     def __init__(self, file_twix, file_protocol=None) -> None:
+        super().__init__()
         self._b0_obj = recoB0(file_twix)
         if file_protocol is not None:
-            self.set_shim_volume(file_protocol) 
+            self.set_protocol(file_protocol) 
         else:
-            self.set_shim_volume(self._b0_obj.hdr['MeasYaps'])
+            self.set_protocol(self._b0_obj.hdr['MeasYaps'])
 
     
     def get_data(self):
         return self._b0_obj
 
-    def calc_shim(self, vol_name:str):
-        shim_volume = self._shim_volume_obj.get(vol_name)
 
+    def calc_shim(self, vol_name:str):
         pass
