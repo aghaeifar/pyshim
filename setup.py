48c62de0b7f9ac96bf9a15f3a7f7a085f40e6c54
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2024-01-16 10:58:12 +0100

    refine

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup
 
 setup(name='pyshim', # this will be name of package in packages list : pip list 
       version='0.1.0',
-      description='shimming toolbox',
+      description='Python-powered toolkit for shimming of B0 and B1 fields in MRI',
       keywords='twix,reconstruction,mri,shimming',
       author='Ali Aghaeifar',
       author_email='ali.aghaeifar [at] tuebingen.mpg [dot] de',
       license='MIT License',
       packages=['pyshim'],
-      install_requires = ['tqdm','numpy','nibabel','torch','twixtools', 'recotwix', 'scipy']
+      install_requires = ['tqdm','numpy','cvxpy']
      )
